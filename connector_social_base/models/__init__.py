553dcd16b0900b0a91be2ec2ce9af4b052e033eb
edescalona <ediliodevelop1991@gmail.com>
2025-04-15 17:09:06 +0100

    [ADD] connector_social_base

---
+++ connector_social_base/models/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2025 Binhex <https://www.binhex.cloud>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import (
+    connector_social_base_mixin,
+    social_network_post_mixin,
+    social_network_account,
+    social_network_media,
+    social_network_post,
+    social_network_post_account,
+    utm_campaign,
+    utm_group_campaign,
+)
---
+++ connector_social_base/tests/test_social_common.py
@@ -0,0 +1,24 @@
+# Copyright 2025 Binhex <https://www.binhex.cloud>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.tests import TransactionCase
+
+PATCH_SOCIAL_BASE_UTILS = "odoo.addons.connector_social_base.social_utils.{}"
+PATCH_SOCIAL_BAS_MODELS = "odoo.addons.connector_social_base.models"
+PATCH_SOCIAL_BASE_MIXIN = (
+    "{}.connector_social_base_mixin.ConnectorSocialBaseMixin.{}".format(
+        PATCH_SOCIAL_BAS_MODELS, "{}"
+    )
+)
+
+
+class TestSocialNetworkCommon(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.SocialMedia = cls.env["social.network.media"]
+        cls.SocialAccount = cls.env["social.network.account"]
+        cls.SocialPost = cls.env["social.network.post"]
+        cls.SocialPostAccount = cls.env["social.network.post.account"]
+        cls.UtmGroupCampaign = cls.env["utm.group.campaign"]
+        cls.UtmCampaign = cls.env["utm.campaign"]
